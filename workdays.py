a5c86fffcc261a7e906c92b439e2600419b623e6
Marko Mahnič <marko.mahnic@gmail.com>
2017-06-08 20:03:22 +0200

    implement workday calculation

---
+++ workdays.py
@@ -0,0 +1,64 @@
+import math
+import datetime as dt
+
+def estimate_end( start, now, total, remaining ):
+    done = total - remaining
+    if done <= 0:
+        return None
+    hours = (now - start).total_seconds() / 3600
+    if hours < 1:
+        return None
+    perhour = float(done) / hours
+    morehours = int(math.ceil(remaining / perhour))
+
+    return now + dt.timedelta( hours = morehours )
+
+
+def _workday_diff( start, end ):
+    if start > end:
+        return 0
+    diff = end-start
+    if diff.days < 1:
+        return diff.seconds / 86400.0
+
+    daydiff = end.weekday() - start.weekday()
+    days = (diff.days - daydiff) / 7 * 5 + min(daydiff,5) - (max(end.weekday() - 4, 0) % 5)
+    return days + diff.seconds / 86400.0
+
+
+def adjusted_start( when ):
+    if when.weekday() <= 4:
+        return when
+    when += dt.timedelta( days = 2 - when.weekday() % 5 )
+    return when.replace( hour=0, minute=0, second=0, microsecond=0 )
+
+
+def adjusted_end( when ):
+    if when.weekday() <= 4:
+        return when
+    when -= dt.timedelta( days = when.weekday() % 4 )
+    return when.replace( hour=23, minute=59, second=59, microsecond=0 )
+
+
+def estimate_end_workdays( start, now, total, remaining ):
+    done = total - remaining
+    if done <= 0:
+        return None
+    # print
+    # print "<--", start, "..", now
+    # print "<--", adjusted_start( start ), "..", adjusted_end( now )
+    days = _workday_diff(adjusted_start(start), adjusted_end(now))
+    if days <= 0:
+        return None
+    perday = float(done) / days
+    moredays = remaining / perday
+    (seconds, moredays) = math.modf( moredays )
+    seconds *= 86400.0
+    nowStart = adjusted_start( now )
+    enddate = nowStart + dt.timedelta( days = moredays, seconds = seconds )
+    offdays = moredays - int(_workday_diff(nowStart, enddate))
+    # print days, "<-->", moredays, seconds
+    # print "-->", nowStart, "..", enddate, "-", offdays
+
+    return adjusted_start( enddate + dt.timedelta( days = offdays ) )
+
--- workdays_t.py
+++ workdays_t.py
@@ -1,52 +1,105 @@
+from workdays import *
 from datetime import datetime, timedelta
 from time import strptime
 import math
+import traceback
 
-def _workday_diff( start, end ):
-    if (end-start).days < 1:
-        return 0
-    daydiff = end.weekday() - start.weekday()
-    days = ((end-start).days - daydiff) / 7 * 5 + min(daydiff,5) - (max(end.weekday() - 4, 0) % 5)
-    return days
+tests=[]
+def test( fn ):
+    tests.append(fn)
+    return fn
 
-def _estimate_end_workdays( start, now, total, remaining ):
-    done = total - remaining
-    if done <= 0:
-        return None
-    # days = (now - start).days 
-    days = _workday_diff(start, now)
-    if days < 1:
-        return None
-    perday = float(done) / days
-    print done, days, perday
-    moredays = int(math.ceil(remaining / perday))
-    enddate = now + timedelta( days = moredays )
-    offdays = moredays - _workday_diff(now, enddate)
-
-    # raise Exception( "days %d, perday %f, moredays %f, enddate %s, offdays %d" % ( days, perday, moredays, enddate, offdays ) )
-    return enddate + timedelta( days = offdays )
+def runTests():
+    for t in tests:
+        print t
+        try: t()
+        except Exception as e:
+            print e
+            traceback.print_exc()
+        print
 
 def _parse_date( datestr ):
     return datetime(*strptime(datestr, "%Y-%m-%d")[0:5]).date()
 
-def test( d1, d2, total, remaining, dexp ):
-    d1 = _parse_date(d1)
-    d2 = _parse_date(d2)
-    dexp = _parse_date(dexp)
-    dres = _estimate_end_workdays( d1, d2, total, remaining )
-    print "expected: %s, actual %s, %s" % (dexp, dres, dres == dexp )
+def _parse_datetime( datestr ):
+    return datetime(*strptime(datestr, "%Y-%m-%d %H:%M")[0:5])
 
-test( "2017-03-06", "2017-03-07", 2, 1, "2017-03-08" )
-test( "2017-03-06", "2017-03-08", 2, 1, "2017-03-10" )
-test( "2017-03-06", "2017-03-09", 2, 1, "2017-03-14" )
-test( "2017-03-06", "2017-03-10", 2, 1, "2017-03-16" )
-test( "2017-03-06", "2017-03-13", 2, 1, "2017-03-20" ) # FIXME: returns -19
+def _is_same_dt( d1, d2, numParts=5 ):
+    return d1.timetuple()[:numParts] == d2.timetuple()[:numParts]
 
-startdate="2017-03-06"
-enddate="2017-03-17"
-import datetime as dt
-import time
-startdate = time.mktime(dt.datetime.combine(_parse_date(startdate), dt.time.min).timetuple())
-enddate   = time.mktime(dt.datetime.combine(_parse_date(enddate), dt.time.max).timetuple())
+@test
+def shouldEstimateEnd():
+    def test( d1, d2, total, remaining, dexp ):
+        d1 = _parse_datetime(d1)
+        d2 = _parse_datetime(d2)
+        dexp = _parse_date(dexp)
+        dres = estimate_end( d1, d2, total, remaining )
+        print "expected: %s, actual %s, %s" % (dexp, dres, _is_same_dt( dres, dexp, 3 ) )
 
-print startdate, enddate
+    # Monday 2017-03-06
+    test( "2017-03-06 00:00", "2017-03-07 00:00", 2, 1, "2017-03-08" )
+    test( "2017-03-06 00:00", "2017-03-08 00:00", 2, 1, "2017-03-10" )
+    test( "2017-03-06 00:00", "2017-03-09 00:00", 2, 1, "2017-03-12" )
+    test( "2017-03-06 00:00", "2017-03-10 00:00", 2, 1, "2017-03-14" )
+    test( "2017-03-06 00:00", "2017-03-13 00:00", 2, 1, "2017-03-20" )
+
+@test
+def shouldAdjustStart():
+    def test( d1, dexp ):
+        dexp = _parse_datetime(dexp)
+        dres = adjusted_start( _parse_datetime( d1 ) )
+        print "expected: %s, actual %s, %s" % (dexp, dres, _is_same_dt( dres, dexp, 5 ) )
+
+    # Monday 2017-03-06
+    test( "2017-03-06 08:00", "2017-03-06 08:00" )
+    test( "2017-03-07 08:00", "2017-03-07 08:00" )
+    test( "2017-03-08 08:00", "2017-03-08 08:00" )
+    test( "2017-03-09 08:00", "2017-03-09 08:00" )
+    test( "2017-03-10 08:00", "2017-03-10 08:00" )
+    test( "2017-03-11 08:00", "2017-03-13 00:00" )
+    test( "2017-03-12 08:00", "2017-03-13 00:00" )
+
+@test
+def shouldAdjustEnd():
+    def test( d1, dexp ):
+        dexp = _parse_datetime(dexp)
+        dres = adjusted_end( _parse_datetime( d1 ) )
+        print "expected: %s, actual %s, %s" % (dexp, dres, _is_same_dt( dres, dexp, 5 ) )
+
+    # Monday 2017-03-06
+    test( "2017-03-06 08:00", "2017-03-06 08:00" )
+    test( "2017-03-07 08:00", "2017-03-07 08:00" )
+    test( "2017-03-08 08:00", "2017-03-08 08:00" )
+    test( "2017-03-09 08:00", "2017-03-09 08:00" )
+    test( "2017-03-10 08:00", "2017-03-10 08:00" )
+    test( "2017-03-11 08:00", "2017-03-10 23:59" )
+    test( "2017-03-12 08:00", "2017-03-10 23:59" )
+
+@test
+def shouldEstimateEndWorkdays():
+    def test( d1, d2, total, remaining, dexp ):
+        d1 = _parse_datetime(d1)
+        d2 = _parse_datetime(d2)
+        dexp = _parse_datetime(dexp)
+        dres = estimate_end_workdays( d1, d2, total, remaining )
+        print "expected: %s, actual %s, %s" % (dexp, dres, _is_same_dt( dres, dexp, 3 ) )
+
+    # Monday 2017-03-06
+    # same week
+    test( "2017-03-06 08:00", "2017-03-07 08:00", 2, 1, "2017-03-08 08:00" )
+    test( "2017-03-06 08:00", "2017-03-08 08:00", 2, 1, "2017-03-10 08:00" )
+    # projection spans weekends
+    test( "2017-03-06 08:00", "2017-03-09 08:00", 2, 1, "2017-03-14 08:00" )
+    test( "2017-03-06 08:00", "2017-03-10 08:00", 2, 1, "2017-03-16 08:00" )
+    # a weekend is in the completed time, estimate falls on weekend
+    # 06 07 08 09 10 w11 w12 13 14 15 16 17 w18 w19 20
+    test( "2017-03-06 08:00", "2017-03-13 08:00", 2, 1, "2017-03-20 08:00" )
+    # Start on weekend
+    test( "2017-03-05 08:00", "2017-03-10 08:00", 2, 1, "2017-03-16 08:00" )
+    test( "2017-03-04 08:00", "2017-03-10 08:00", 2, 1, "2017-03-16 08:00" )
+    # Start and now on weekend
+    test( "2017-03-05 08:00", "2017-03-11 08:00", 2, 1, "2017-03-17 23:59" )
+    test( "2017-03-04 08:00", "2017-03-12 08:00", 2, 1, "2017-03-17 23:59" )
+
+
+runTests()
