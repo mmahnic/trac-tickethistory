551188bd4fb1728d39bc3fda58b5fe8c744275a0
Marko Mahnič <marko.mahnic@gmail.com>
2017-06-19 18:57:07 +0200

    add the year to the copyright notice

--- tickethistory/dbutils.py
+++ tickethistory/dbutils.py
@@ -1,4 +1,4 @@
-## Copyright (c) Marko Mahnič. All rights reserved.
+## Copyright (c) 2017 Marko Mahnič. All rights reserved.
 ## Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 from trac.ticket.query import Query
